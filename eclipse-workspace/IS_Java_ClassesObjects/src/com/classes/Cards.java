32488b215b17395b3542a19be5a51378090d7040
SBantu5 <sandhya.bantu44@gmail.com>
2021-11-22 14:53:39 -0500

    commit

---
+++ eclipse-workspace/IS_Java_ClassesObjects/src/com/classes/Cards.java
@@ -0,0 +1,40 @@
+package com.classes;
+
+public class Cards {
+
+		   private String suit;
+		   private String rank;
+
+		public Cards() {
+		    this.suit = suit;
+		    this.rank = rank;
+		}
+	
+	 public static void main(String[] args) {
+	
+
+}
+	 //Enum representation
+	 public enum Suit {
+		    SPADES,
+		    HEARTS,
+		    DIAMONDS,
+		    CLUBS;
+		}
+
+		public enum Rank {
+		    TWO,
+		    THREE,
+		    FOUR,
+		    FIVE,
+		    SIX,
+		    SEVEN,
+		    EIGHT,
+		    NINE,
+		    TEN,
+		    JACK,
+		    QUEEN,
+		    KING,
+		    ACE;
+		}
+}
